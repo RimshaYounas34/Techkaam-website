f223e9a8bbb8b06757e3902be27d18ba96de4e4f
Rimsha <mahersaba441@gmail.com>
2026-06-11 07:09:34 +0500

    fix middleware auth and routing issues

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,3 +1,4 @@
+const auth = require("../middleware/auth");
 const router = require("express").Router();
 
 const {
@@ -12,6 +13,7 @@ router.post("/create", createAdmin);
 router.post("/login", loginAdmin);
 
 // MSG SHOW KRWANY KY LIY
-router.get("/messages", getMessages);
+router.get("/messages", auth, getMessages);
+router.delete("/messages/:id", auth, deleteMessage);
 
 module.exports = router;
